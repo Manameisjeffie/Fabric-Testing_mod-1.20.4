32c830490abe2fac497d8d93192e4fd191dd4b5f
Jeffie <banaemohamed@gmail.com>
2024-04-04 07:05:10 +0000

    Removed Happy face from ingedient creative tab

--- src/main/java/net/jeffie/testingmod/item/ModItems.java
+++ src/main/java/net/jeffie/testingmod/item/ModItems.java
@@ -1,11 +1,8 @@
 package net.jeffie.testingmod.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.jeffie.testingmod.TestingMod;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
